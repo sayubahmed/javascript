b63e7e719488233527090636df68140b7eb5f16d
sayubahmed <ayub58377@gmail.com>
2023-12-14 10:59:53 -0800

    second commit

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
-let a=2
-let b=3
+var a=2
+var b=3
 if(a>=b){
-     console.log("it is not an even no")
+     console.log(" it is not an even no")
 }else{
-   console.log(" it is an even no")
+   console.log(" 1st-it is an even no")
 }
 console.log(a>=b)
+
+var a=3
+var b=4
+if(a<=b){
+    console.log(" 2nd-3 is an odd no")
+}else{
+    console.log("4 is not an odd no")
+}
+var a= 500
+var b=400
+
+if(a>=b ){
+    console.log(" 3rd-no output")
+}else{
+console.log("A")
+}
+var a=71
+var b=60
+var c=100
+
+if(a<=c && b<=a && c>=a){
+    console.log(" average")
+    console.log("fail")
+    console.log("excellent")
+}else{
+    console.log("no output")
+}
+var a=15
+var b=20
+var c=3
+
+if(a<=b && b>=a && c<=a){
+    console.log("hi five")
+    console.log("hi five")
+    console.log("better luck next time")
+}else{
+    console.log("no output")
+}
